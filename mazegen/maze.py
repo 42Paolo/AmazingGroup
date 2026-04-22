4827b4cfae49399f4a451639cc75ca20d3571b7f
42Paolo <emailcreatasulmomentoo@gmail.com>
2026-04-22 13:23:08 +0200

    add: neighbors

--- mazegen/maze.py
+++ mazegen/maze.py
@@ -1,3 +1,9 @@
+
+W = 1  # su
+D = 2  # destra
+S = 4  # giù
+A = 8  # sinistra 
+
 class Maze:
 	def __init__(self, width:int, height:int, entry: tuple[int, int], exit_: tuple[int, int]) -> None:
 			self.width = width
@@ -5,7 +11,7 @@ class Maze:
 			self.entry = entry
 			self.exit_ = exit_
 			self.grid = [[0] * width for _ in range(height)]
-	#BITWISE OPERATION
+
 	def has_wall(self, x, y, direction):
 		return bool(self.grid[y][x] & direction)
 
@@ -18,7 +24,17 @@ class Maze:
 	def in_bounds(self, x, y):
 		return 0 <= x < self.width and 0 <= y < self.height
 
-	
+	def neighbors(self, x, y):
+		directions = [
+			(x,   y-1, W, S),
+			(x+1, y,   D, A),  
+			(x,   y+1, S, W),  
+			(x-1, y,   A, D),  
+		]
+		# returna solo le celle che esistono nella griglia per eveitare di rendere il perimetro aperto
+		return [(nx, ny, d, opp) for nx, ny, d, opp in directions if self.in_bounds(nx, ny)]
+
+
 
 
 #   Ricorda che ogni cella è un intero, e le direzioni corrispondono a bit:                         
