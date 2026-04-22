a35a242d6fca31bbd95963edbb021bea1a1cb2ab
42Paolo <emailcreatasulmomentoo@gmail.com>
2026-04-22 13:38:01 +0200

    add: correzione commenti

--- mazegen/maze.py
+++ mazegen/maze.py
@@ -33,29 +33,3 @@ class Maze:
 		]
 		# returna solo le celle che esistono nella griglia per eveitare di rendere il perimetro aperto
 		return [(nx, ny, d, opp) for nx, ny, d, opp in directions if self.in_bounds(nx, ny)]
-
-
-
-
-#   Ricorda che ogni cella è un intero, e le direzioni corrispondono a bit:                         
-#   - N = bit 0
-#   - E = bit 1                                                                                     
-#   - S = bit 2                                               
-#   - W = bit 3
-             
-#  0  = nessuna parete                                                          
-#   1  = N          ↑                                                            
-#   2  = E            →                                         
-#   3  = N+E        ↑ →
-#   4  = S              ↓                                                        
-#   5  = N+S        ↑   ↓
-#   6  = E+S          → ↓                                                        
-#   7  = N+E+S      ↑ → ↓                                                        
-#   8  = W          ←
-#   9  = N+W        ↑ ←                                                          
-#   10 = E+W          → ←
-#   11 = N+E+W      ↑ → ←                                                        
-#   12 = S+W          ↓ ←                                                        
-#   13 = N+S+W      ↑ ↓ ←
-#   14 = E+S+W        → ↓ ←                                                      
-#   15 = N+E+S+W    ↑ → ↓ ←  
